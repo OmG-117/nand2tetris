a6e350d2063e6e1b8610c2535fbc96fad97f03c5
OmG-117 <im.omgupta@gmail.com>
2021-05-10 01:37:47 +0530

    Actually do things

--- jackcompiler/jackanalyzer.py
+++ jackcompiler/jackanalyzer.py
@@ -1,22 +1,47 @@
-import glob
+import os
 import sys
+
+from compengine import compile_file
+from tokenizer import TokenList
 
 
 def main():
     # Get the target directory from the command line args.
     input_directory = sys.argv[1]
 
-    # Get a list of all Jack files in the supplied directory. If no Jack files
-    # exist or if the path is invalid, raise an error.
-    target_files = [f for f in glob.glob(input_directory + '*.jack')]
-    if not target_files:
-        raise ValueError('Invalid directory name')
+    # Get a list of all Jack files in the supplied directory.
+    print(f'Looking for .jack files in {input_directory}')
+    target_paths = [
+        os.path.join(input_directory, f)
+        for f in os.listdir(input_directory)
+        if f.endswith('.jack')
+    ]
+
+    # If there are no .jack files, raise an error
+    if not target_paths:
+        raise ValueError('No .jack files in specified directory')
+    else:
+        print(f'Compiling {len(target_paths)} files in {input_directory}')
 
     # Iterate through each target file and compile it separately.
-    for target_file in target_files:
-        output_file = target_file[ :-5] + '.xml'
-        print(target_file, output_file)
+    for target_path in target_paths:
+        print(f'Analyzing {target_path}')
+        with open(target_path, 'r') as f:
+            target_contents = f.read()
+        
+        # Tokenize the code and generate the parse tree
+        try:
+            token_list = TokenList(target_contents)
+            parse_tree = compile_file(token_list)
+        except ValueError as exc:
+            raise SystemExit(exc)
 
+        # Write the parse tree to a .xml file
+        output_path = os.path.splitext(target_path)[0] + '_gen.xml'
+        with open(output_path, 'w') as f:
+            f.write(parse_tree.as_xml(2, True)[1: ] + '\n')
+
+        print(f'Wrote parsed output to {output_path}')
 
 if __name__ == '__main__':
     main()
