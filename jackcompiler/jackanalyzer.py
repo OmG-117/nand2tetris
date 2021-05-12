983933b1cf0da7ab82c625f72e45cb91b039a9a5
OmG-117 <im.omgupta@gmail.com>
2021-05-13 01:12:09 +0530

    Add handling for single file targets

--- jackcompiler/jackanalyzer.py
+++ jackcompiler/jackanalyzer.py
@@ -7,21 +7,25 @@ from tokenizer import TokenList
 
 def main():
     # Get the target directory from the command line args.
-    input_directory = sys.argv[1]
+    input_path = sys.argv[1]
 
-    # Get a list of all Jack files in the supplied directory.
-    print(f'Looking for .jack files in {input_directory}')
-    target_paths = [
-        os.path.join(input_directory, f)
-        for f in os.listdir(input_directory)
-        if os.path.splitext(f)[1] == '.jack'
-    ]
-
-    # If there are no .jack files, raise an error
-    if not target_paths:
-        raise ValueError('No .jack files in specified directory')
+    if os.path.isdir(input_path):
+        # Get a list of all Jack files in the supplied directory.
+        print(f'Looking for .jack files in {input_path}')
+        target_paths = [
+            os.path.join(input_path, f)
+            for f in os.listdir(input_path)
+            if os.path.splitext(f)[1] == '.jack'
+        ]
+        # If there are no .jack files, raise an error
+        if not target_paths:
+            raise ValueError('No .jack files in specified directory')
+        else:
+            print(f'Compiling {len(target_paths)} file(s) at {input_path}')
     else:
-        print(f'Compiling {len(target_paths)} files in {input_directory}')
+        # Store the path of the provided file in a list
+        print(f'Compiling file at {input_path}')
+        target_paths = [input_path]
 
     # Iterate through each target file and compile it separately.
     for target_path in target_paths:
